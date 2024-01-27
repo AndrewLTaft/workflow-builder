04b1038b0f38b88dff18b8c4fbd084c236c28d06
Andrew Taft <andrew.taft@taftdev.com>
2024-01-27 13:10:26 -0500

    Update part and workflow models

--- angular-net/backend/Api/dataaccess/Models/Part.cs
+++ angular-net/backend/Api/dataaccess/Models/Part.cs
@@ -5,5 +5,12 @@ public class Part
   public int Id { get; set; }
 
   public required int WorkflowId { get; set; }
+
   public Workflow? Workflow { get; set; }
+
+  public bool Completed { get; set; } = false;
+
+  public int? StepId { get; set; }
+
+  public Step? Step { get; set; }
 }
--- angular-net/backend/Api/dataaccess/Models/Workflow.cs
+++ angular-net/backend/Api/dataaccess/Models/Workflow.cs
@@ -3,10 +3,10 @@ namespace Api.DataAcess.Models;
 
 public class Workflow
 {
-  public int Id { get; set; }
+  public int? Id { get; set; }
   public required string Name { get; set; }
   public string? Description { get; set; }
 
-  public ICollection<Step> Steps { get; set; } = new List<Step>();
+  public List<Step> Steps { get; set; } = new List<Step>();
   public ICollection<Part> Parts { get; set; } = new List<Part>();
 }
